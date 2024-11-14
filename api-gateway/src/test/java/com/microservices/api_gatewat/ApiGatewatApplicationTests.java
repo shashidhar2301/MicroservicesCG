fc2922cfd86b0ad3c492426c4ce090a107702e03
RAJ <alli-shashidhar.raj@capgemini.com>
2024-11-14 17:40:29 +0530

    API gateway

---
+++ api-gateway/src/test/java/com/microservices/api_gatewat/ApiGatewatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.api_gatewat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
